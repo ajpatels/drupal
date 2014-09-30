f8ca1c6cb9aaffacb2fb1582ac92b193d7bdd28a
ajpatels <ankitpatel1286@gmail.com>
2014-09-30 17:20:44 -0400

    updated by ankit
    updated setting.php

--- settings.php
+++ settings.php
@@ -3,4 +3,24 @@
 /**
  * You need to configure the site name here. This will be used to load the correct settings file on the Acquia servers.
  */
+?><?php
+
+/**
+ * You need to configure the site name here. This will be used to load the correct settings file on the Acquia servers.
+ */
+?><?php
+
+/**
+ * You need to configure the site name here. This will be used to load the correct settings file on the Acquia servers.
+ */
+?><?php
+
+/**
+ * You need to configure the site name here. This will be used to load the correct settings file on the Acquia servers.
+ */
+?><?php
+
+/**
+ * You need to configure the site name here. This will be used to load the correct settings file on the Acquia servers.
+ */
 ?>
